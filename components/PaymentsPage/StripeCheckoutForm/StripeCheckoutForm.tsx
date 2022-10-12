00e7cd969549b93023c68d8f15c89abd30a7bd23
mujeex <mujahid.bappai@yahoo.com>
2022-10-12 07:42:16 +0100

    feat: add myBookings page boilerplate

--- components/PaymentsPage/StripeCheckoutForm/StripeCheckoutForm.tsx
+++ components/PaymentsPage/StripeCheckoutForm/StripeCheckoutForm.tsx
@@ -24,7 +24,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: "https://example.com/order/123/complete",
+        return_url: "https://localhost:3002/myBookings",
       },
       
     });
