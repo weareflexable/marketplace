5148e589b37cc8efbbba708f53a6a31f715c4a97
mujeex <mujahid.bappai@yahoo.com>
2022-10-12 08:05:52 +0100

    feat: add tabs to bookings page

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Flex,Box,Heading,SimpleGrid,VStack,Grid,GridItem} from '@chakra-ui/react'
+import {Flex,Box,Heading,SimpleGrid,VStack,Grid,GridItem,Button,Tabs, TabList, TabPanels, Tab, TabPanel} from '@chakra-ui/react'
 
 
 const purchasedTickets = [
@@ -36,7 +36,24 @@ export default function MyBookings(){
             <GridItem colStart={2} colEnd={4}>
                 <Flex w='600px' direction='column'>
                     <Heading mt='10' mb='6'>My Bookings</Heading>
-                    <PurchasedTickets/>
+                    <Tabs variant='soft-rounded'>
+                        <TabList>
+                            <Tab>Purchased</Tab>
+                            <Tab>Redeemed</Tab>
+                            <Tab>Expired</Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <PurchasedTickets/>
+                            </TabPanel>
+                            <TabPanel>
+                                <PurchasedTickets/>
+                            </TabPanel>
+                            <TabPanel>
+                                <PurchasedTickets/>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
                 </Flex>
             </GridItem>
         </Grid>
@@ -46,8 +63,7 @@ export default function MyBookings(){
 
 const PurchasedTickets = ()=>{
     return(
-    <Box p='1em' w='100%'  border='1px solid #e5e5e5' >
-        <Heading as='h2' mb='4' size='md'> Redeemed | Expired </Heading>
+    <Box w='100%' mt='3' >
         <SimpleGrid columns={1} spacing='3'>
             {purchasedTickets.map((ticket)=>(
                 <Ticket key={ticket.id} data={ticket}/>
@@ -66,11 +82,17 @@ const Ticket = ({data})=>{
                     {data.productName}
                 </Box>    
                 <Box>
+                    {data.description}
+                </Box>
+                <Box>
                     ${data.price}
                     <Box as='span' color='gray.600' fontSize='sm'>
                         /person
                     </Box>
                 </Box>
+                <Button>
+                    Redeem Ticket
+                </Button>
             </VStack>
         </Box>
     )
