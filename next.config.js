8516029c3ed426e92a63c975dc354746421327ca
thisisommore <ommore501@gmail.com>
2022-10-26 18:39:11 +0000

    Ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env:{
-    STRIPE_PUBLIC_KEY: 'pk_live_51LkqgnLY9m0w00gpcdDH7JaOEXQo3DYhOiLfA8Eebg2ZQhuzGpxYu9PEOcnU9qrQIjn1OyJQO9nW01GrrXfqXaZF002uirDAjV'
-  }
-}
+  env: {
+    STRIPE_PUBLIC_KEY:
+      "pk_live_51LkqgnLY9m0w00gpcdDH7JaOEXQo3DYhOiLfA8Eebg2ZQhuzGpxYu9PEOcnU9qrQIjn1OyJQO9nW01GrrXfqXaZF002uirDAjV",
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
