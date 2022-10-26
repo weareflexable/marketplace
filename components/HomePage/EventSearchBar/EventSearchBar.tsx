829a35deac750474f4777437f5a0b5b6f7d94794
thisisommore <ommore501@gmail.com>
2022-10-26 18:53:53 +0000

    EventSearchBar: Comment error code

--- components/HomePage/EventSearchBar/EventSearchBar.tsx
+++ components/HomePage/EventSearchBar/EventSearchBar.tsx
@@ -1,42 +1,49 @@
-import React,{useState} from 'react'
-import {Box,Input,HStack,Button,FormControl,Select,FormErrorMessage} from '@chakra-ui/react'
-import States from 'usa-states'
+import React, { FormEventHandler, useState } from "react";
+import {
+  Box,
+  Input,
+  HStack,
+  Button,
+  FormControl,
+  Select,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import States from "usa-states";
 
+export default function EventSearchBar() {
+  const usa = new States.UsaStates();
+  const handleSearchEvent: FormEventHandler<HTMLDivElement> | undefined = (
+    event
+  ) => {
+    event.preventDefault();
+    // const country = event.target[0].value;
+    // const state = event.target[1].value;
+  };
 
-export default function EventSearchBar(){
+  return (
+    <Box p=".5em" margin={"1em auto"} bg="#ffffff" borderRadius="4px">
+      <HStack as="form" onSubmit={handleSearchEvent} spacing={2}>
+        <FormControl w="250px" mr={0} isRequired>
+          <Select placeholder="Select your country">
+            <option defaultValue={"United States"} value="US">
+              United States
+            </option>
+          </Select>
+          <FormErrorMessage>Country is required</FormErrorMessage>
+        </FormControl>
 
-
-
-  const usa = new States.UsaStates()
-
-  const handleSearchEvent = (event: { preventDefault: () => void; target: { value: any }[] })=>{
-    event.preventDefault()
-    const country = event.target[0].value;
-    const state = event.target[1].value
-
-  }
-
-    return(
-        <Box p='.5em'  margin={'1em auto'} bg='#ffffff' borderRadius='4px'>
-
-            <HStack as='form' onSubmit={handleSearchEvent} spacing={2}>
-                <FormControl w='250px' mr={0} isRequired>
-                    <Select  placeholder='Select your country'>
-                      <option defaultValue={'United States'} value='US'>United States</option>
-                    </Select>
-                    <FormErrorMessage>Country is required</FormErrorMessage>
-                  </FormControl>
-
-                  <FormControl w='250px' mr={0} isRequired>
-                    <Select placeholder='Select your state'>
-                        {usa.states.map(state=>(
-                          <option key={state.abbreviation} value={state.name.toLowerCase()}>{state.name} - {state.abbreviation}</option>
-                          ))}
-                    </Select>
-                    <FormErrorMessage>State is required</FormErrorMessage>
-                  </FormControl>
-                  <Button type='submit'>Search for Bars</Button>  
-            </HStack>
-        </Box>
-    )  
+        <FormControl w="250px" mr={0} isRequired>
+          <Select placeholder="Select your state">
+            {usa.states.map((state) => (
+              <option key={state.abbreviation} value={state.name.toLowerCase()}>
+                {state.name} - {state.abbreviation}
+              </option>
+            ))}
+          </Select>
+          <FormErrorMessage>State is required</FormErrorMessage>
+        </FormControl>
+        <Button type="submit">Search for Bars</Button>
+      </HStack>
+    </Box>
+  );
 }
