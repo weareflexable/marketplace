6713c5c75973794dd004ad523b4ab2eef2094867
mujeex <mujahid.bappai@yahoo.com>
2022-11-05 16:13:50 +0100

    fix: add dayjs import in qrModal

--- components/BookingsPage/QrCodeModal/QrCodeMobile/QrCodeMobile.tsx
+++ components/BookingsPage/QrCodeModal/QrCodeMobile/QrCodeMobile.tsx
@@ -1,7 +1,7 @@
 import {useEffect} from 'react'
 import { Modal, Text, Box, Image, DrawerCloseButton, Skeleton, Flex, HStack, VStack, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Divider } from "@chakra-ui/react"
 import QRCode from "react-qr-code";
-
+import dayjs from 'dayjs';
 
 
 interface QrCodeMobileProps{
