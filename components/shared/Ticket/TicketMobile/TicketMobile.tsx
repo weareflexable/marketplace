976667fd6fa3aaf153d5742f796786891a98ef8a
mujeex <mujahid.bappai@yahoo.com>
2022-12-23 11:42:41 +0100

    refactor: replace bottom panel with flexable button combo in mobile ticket

--- components/shared/Ticket/TicketMobile/TicketMobile.tsx
+++ components/shared/Ticket/TicketMobile/TicketMobile.tsx
@@ -26,7 +26,6 @@ function TicketMobile ({data,selectedDate, onTriggerAction}:TicketProps){
     
     return( 
         <Box display={['block','block','none']} bg='blackAlpha.700' cursor='pointer' >
-            {/* <Image src='/assets/placeholder.jpeg' style={{height:'150px', width:'100%' }} alt={data.thumbnailAlt} width='100' height='150' /> */}
             <Flex direction='column'>
                 <Flex py='1em'>
                     <Flex px='1em' flex={4} direction='column'>
@@ -37,55 +36,14 @@ function TicketMobile ({data,selectedDate, onTriggerAction}:TicketProps){
                         <Text textStyle={'secondary'}>
                             {data.description}
                         </Text>
+                        <Text>${data.price}</Text>
                     </Flex>
-                    <Flex mr='2' flex='1' direction='column' justifyContent={'center'} alignItems='center'>
-                         {/* <Text mb='3' textStyle={'ticketPrice'}>${data.price/100}</Text>  */}
-                         {/* <HStack mt='2' spacing='2'>
-                            <IconButton disabled={isMinQuantity} onClick={isMinQuantity?()=>{}:decrementQuantity} color={isMinQuantity?'cyan.50':'cyan.400'} size='sm' icon={<MdRemove/>} aria-label='remove-item'/>
-                            <Text textStyle={'caption'} color={isMinQuantity?'whiteAlpha.500':'whiteAlpha.800'}>{ticketData.quantity}</Text>
-                            <IconButton onClick={incrementQuantity} size='sm' color='cyan.400' icon={<MdAdd/>} aria-label='increment-item-quantity'/>
-                        </HStack> */}
-                    </Flex> 
                 </Flex>
                 
 
                 {/* bottom panel */}
-
                 <Flex px='1em' py='.5em' alignItems='center' justifyContent='space-between' bg='gray.800'>
-
                     <FlexableComboButton ticketData={data}/>
-                {/* //    <>
-                //    <HStack spacing={3}>
-                //         <HStack spacing='1'>
-                //             <Text color='gray.500' textStyle={'caption'} >
-                //                 Valid on 
-                //             </Text>
-                //             <Text textStyle={'caption'}>
-                //             {dayjs(selectedDate).format('MMM DD, YYYY')} 
-                //             </Text> 
-                //         </HStack>
-                    
-                //     </HStack>
-                //     <Flex alignItems='center' justifyContent='center'>
-                //         {isTicketsSoldOut
-                //         ?<Text color={'gray.500'} textStyle={'body'}>Sold out</Text>
-                //         :<>
-                //          <HStack mr='2' spacing='2'>
-                //              <Text textStyle={'caption'} color='gray.500'>Tickets left</Text>
-                //              <Text textStyle={'caption'}>{ticketData.tickets[0]!.ticketsAvailable}</Text>
-                //          </HStack>
-                //          <HStack spacing='1'>
-                //            <IconButton onClick={()=>onTriggerAction(data.id)} color={'cyan.400'} size='sm' icon={<MdAddShoppingCart color='cyan'/>} aria-label='remove-item'/>
-                //            <Button disabled={isMinQuantity&&isAuthenticated} size={'sm'} mr='2' onClick={buyTicketNow}>
-                //              <Text color='cyan' textStyle='caption'>{`Buy Now $${subTotal}`}</Text> 
-                //            </Button>
-                //          </HStack>
-                        
-                //          </>  
-                //          }
-                //     </Flex>
-                //     </>
-                    // :<Text>Tickets are not available on selected date</Text>   } */}
                 </Flex>
             </Flex>
         </Box>
