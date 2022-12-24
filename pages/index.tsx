c4b26cfc711ebce54c7863f52346316c3551968d
mujeex <mujahid.bappai@yahoo.com>
2022-12-24 09:26:03 +0100

    feat: show loading skeleton when services are loaded

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import EventSearchBar from '../components/HomePage/EventSearchBar/EventSearchBar'
 import StoreCard from '../components/HomePage/StoreCard/StoreCard'
-import {Flex,Skeleton,Text,Wrap,WrapItem} from '@chakra-ui/react'
+import {Flex, Text, Wrap, WrapItem} from '@chakra-ui/react'
 import Layout from '../components/shared/Layout/Layout'
 import {Store} from '../Types/Stores.types'
 import {useQuery} from '@tanstack/react-query'
 import { useRouter } from 'next/router'
 import { mockData } from '../data/events'
-
+import SkeletonList from '../components/HomePage/SkeletonList/SkeletonList'
 
 
 
@@ -49,16 +49,7 @@ export default function Home() {
                   <Text  as='h1' w='100' textStyle={'h3'}>Showing you bars in Syracuse NY</Text>
                 </Flex>
 
-                <Wrap w='100%' padding={[3,5]} spacing={5} alignItems='center' justifyContent='center'> 
-                    {/* {data && data.payload ? data.payload.map((store:Store)=>( */}
-                      {mockData? mockData.map((store:Store)=>(
-                        <WrapItem flexGrow={'1'} flexBasis={['100%','22%']} maxWidth={['100%','24%']} key={store.id}>
-                            {/* <Skeleton w={'100%'} isLoaded={!isLoading}> */}
-                              <StoreCard data={store}/>
-                            {/* </Skeleton> */}
-                        </WrapItem> 
-                    )):null}
-                </Wrap>
+                <SkeletonList/>
 
          </Layout>
          </>
