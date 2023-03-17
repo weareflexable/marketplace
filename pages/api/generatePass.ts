8a8c2dd7ec76d219fddfb1bfa232295bca3581ec
mbappai <mujahid.bappai@yahoo.com>
2023-03-17 11:31:34 +0100

    fix: Update dayjs imports

--- pages/api/generatePass.ts
+++ pages/api/generatePass.ts
@@ -81,7 +81,7 @@ export default async function handler(req:any, res:NextApiResponse){
         value: eventName,
         textAlignment: "PKTextAlignmentLeft",
     }) 
-
+ 
     pass.setExpirationDate(new Date(expiryDate))
 
     // pass.type = "eventTicket"
