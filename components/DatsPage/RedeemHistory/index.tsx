1d26cb767376db17fd6bbd0abf65dd891d5dd73c
mbappai <mujahid.bappai@yahoo.com>
2023-06-02 08:14:43 +0100

    refactor: pass bookingId query param to redeem history

--- components/DatsPage/RedeemHistory/index.tsx
+++ components/DatsPage/RedeemHistory/index.tsx
@@ -32,13 +32,12 @@ export default function RedeemHistory({id,quantity,type}:Props){
 
     const {paseto} = useAuthContext()
 
-    const urlSuffix = type === 'community' ? `communityId=${id}`: `serviceId=${id}`
 
 
     const redeemHistoryQuery = useQuery({
         queryKey:['redeem-history', id], 
         queryFn:async()=>{
-            const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users/tickets/redeem-history?${urlSuffix}`,{
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users/tickets/redeem-history?bookingId${id}`,{
                 headers:{
                     "Authorization": paseto
                 }
