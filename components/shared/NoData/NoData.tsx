bcca99e0a479a9aabd669d5861e300604cf35042
mbappai <mujahid.bappai@yahoo.com>
2023-06-18 09:51:52 +0100

    refactor: reduce minHeight of empty state for noDATs

--- components/shared/NoData/NoData.tsx
+++ components/shared/NoData/NoData.tsx
@@ -9,12 +9,12 @@ export default function NoData(){
         router.push('/')
     }
     return(
-    <Flex justifyContent='center' bg='#121212' alignItems='center' height='100%' minHeight='100vh' width={"100%"}>
-        <Flex direction='column' maxW={'350px'} alignItems='center'>
+    <Flex justifyContent='center' bg='#121212' alignItems='center' height='100%' minHeight='60vh' width={"100%"}>
+        <Flex direction='column' maxW={'320px'} alignItems='center'>
             <Text as='h3' mb='5' textStyle={'h3'}>
                 No DATS!
             </Text>
-            <Text mb='3' textAlign={'center'} textStyle={'body'} color='text.200'>
+            <Text mb='5' textAlign={'center'} textStyle={'body'} color='text.200'>
                {` Seems like you haven't booked any DAT yet`}
             </Text>
             <Button variant='ghost' onClick={navigateToMarketPlace}>
