e486df3da928d9f10b50320b4a4909ca77e80ba7
mbappai <mujahid.bappai@yahoo.com>
2023-09-05 08:46:27 +0100

    feat: add social share buttons

--- utils/socialShare.ts
+++ utils/socialShare.ts
@@ -1,14 +1,30 @@
 const handleShareTwitter = () => {
-    const text = encodeURIComponent("Check out this community event from flexable!!");
+    const text = encodeURIComponent("Check this out from flexable!!");
     const twitterShareLink = `https://twitter.com/intent/tweet?text=${text}&url=${window.location.href}`;
     window.open(twitterShareLink, "_blank");
   };
 
   const handleShareLinkedIn = () => {
     const url = encodeURIComponent(window.location.href);
-    const title = encodeURIComponent("Check out this community event from flexable!");
+    const title = encodeURIComponent("Check this out from flexable!");
     const linkedInShareLink = `https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}`;
     window.open(linkedInShareLink, "_blank");
   };
 
-export {handleShareLinkedIn, handleShareTwitter}
+  const handleShareFacebook = () => {
+    const url = encodeURIComponent(window.location.href);
+    const title = encodeURIComponent("Check this out from flexable!");
+    const shareLink = `https://www.facebook.com/sharer.php?u=${url}`;
+    window.open(shareLink, "_blank");
+  };
+
+
+  const handleShareWhatsapp = () => {
+    const url = encodeURIComponent(window.location.href);
+    const title = encodeURIComponent("Check this out from flexable!");
+    const shareLink = `whatsapp://shareLink?text=${title}&url=${url}`;
+    window.open(shareLink, "_blank");
+  }; 
+
+
+export {handleShareLinkedIn, handleShareTwitter, handleShareWhatsapp, handleShareFacebook}
