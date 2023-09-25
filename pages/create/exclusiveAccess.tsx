6b9bddd10f268399c72509564e561b1e74b8b620
mbappai <mujahid.bappai@yahoo.com>
2023-09-25 18:05:11 +0100

    fix: update link to portal

--- pages/create/exclusiveAccess.tsx
+++ pages/create/exclusiveAccess.tsx
@@ -261,7 +261,7 @@ function BasicForm({prev,next}:StepProps){
                 </Flex>
                : userOrgsQuery?.data?.length < 1
                ? <Flex p={8} justifyContent={'center'} alignItems={'center'} border={'1px solid'}>
-                    <Text textAlign={'center'} color={'text.200'} textStyle={'body'} width={'100%'}>It seems like you are not a part of any organization. Get started creating one on <Link color={'brand.300'} target="_blank" textDecoration={'underline'} colorScheme="brand" href={`https://portal.dev.flexabledats.com`}>flexable portal</Link></Text> 
+                    <Text textAlign={'center'} color={'text.200'} textStyle={'body'} width={'100%'}>It seems like you are not a part of any organization. Get started creating one on <Link color={'brand.300'} target="_blank" textDecoration={'underline'} colorScheme="brand" href={`https://portal.flexabledats.com`}>flexable portal</Link></Text> 
                 </Flex>
                :<Box>
                     <FormControl mb={'1rem'} px={['1rem']} w={['90%','100%','70%']}>
