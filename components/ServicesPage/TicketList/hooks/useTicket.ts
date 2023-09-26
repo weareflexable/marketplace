82a6466cf87774dc50e8a058d53b966f4fcb365a
mbappai <mujahid.bappai@yahoo.com>
2023-09-26 17:43:18 +0100

    refactor: replace payment intent APIs with user api

--- components/ServicesPage/TicketList/hooks/useTicket.ts
+++ components/ServicesPage/TicketList/hooks/useTicket.ts
@@ -80,7 +80,7 @@ const useTicket = (data:any)=>{
 
      async function fetchSecret(payload:any) {
           try{
-          const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${roleName}/payment-intents/buy-now`,payload,{
+          const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/payment-intents/buy-now`,payload,{
             headers:{
               'Authorization': paseto
             }
